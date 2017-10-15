114a45f9e017fddaccab0ed22a496b1fefa0cf74
damanusus2 <damiantomilo@gmail.com>
2017-10-15 17:11:42 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<html lang="pl">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href='https://fonts.googleapis.com/css?family=Sofia' rel='stylesheet'>
+		<link rel="stylesheet" href="css/bootstrap.css">	
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/animate.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="js/bootstrap.js"></script>
+		<script src="js/jquery.textillate.js"></script>
+		<script src="js/jquery.lettering.js"></script>
+		<script src="js/jquery.fittext.js"></script>
+		<script src="js/parallax.min.js"></script>
+	</head>
+	<body >
+	<header>
+			<div id="home" class="container-fluid">
+					<div class="nav">
+						<div class="menu-container">
+							<div class="menu-elements">
+
+								<div class="img-menu-1">
+									<a href="#home"><img src="img/home-active.png" class="img-fluid image"></a>		
+								</div>
+								<div class="img-menu-2"><a href="#omnie"><img src="img/omnie-active.png" class="img-fluid image"></a></div>
+								<div class="img-menu-3"><a href="#portfolio"><img src="img/portfolio-active.png" class="img-fluid image"></a></div>
+								<div class="img-menu-4"><a href="#kontakt"><img src="img/kontakt-active.png" class="img-fluid image"></a></div>
+							</div>
+						</div>
+				   </div>
+			</div>
+		</header>
+
+		<div class=" start-page">
+			<h1 class="tlt" data-in-effect="rollIn" style="font-family: 'Aguafina Script';">Tomiło Design</h1>
+			<h1 class="tlt2" data-in-effect="fadeInUp">Spraw aby twój projekt poznał każdy</h1>
+			<div class='arrow-slide'><a href="#home"><i class="fa fa-angle-double-down"></i></a></div> 
+		</div>
+		
+		<div class="clear-space"></div>
+	   <div class="container-fluid">
+			<div class="row">
+				<div class="col-md-8 offset-md-2 col-sm-10 offset-sm-1 content">
+					<div  class=" content-div omnie">
+						<h1 id="omnie">O mnie</h1>
+					<div class="row about-me-element">
+						<div class="col-md-6 col-sm-6 col-xs-12 text-center">
+							<p align="justify">Jestem studentem informatyki, który w wolnym czasie zajmuje się projektami poza uczelnią. Lubię poświęcać czas na zdobywanie nowych umiejętności, jak również pogłębianie tych już posiadanych. Chcę się rozwijać w dziedzinie web developmentu, ale również jestem otwarty na nowe ścieżki rozwoju</p>
+						</div>
+						<div class="col-md-6 col-sm-6 col-xs-12 text-center">
+						<img src="img/aboutme1.png" class="about-img img-fluid ">
+						</div>
+					</div>
+					<div class="row about-me-element">
+						<div class="col-md-6 col-sm-6 col-xs-12 text-center">
+							<img src="img/aboutme2.png" class="about-img img-fluid ">
+						</div>
+						<div class="col-md-6 col-sm-6 col-xs-12 text-center">
+							<p align="justify">Dodatkowo zajmuję się również podstawową grafiką na własnę potrzeby z wykorzystaniem Adobe PhotoShop. Jestem w stanie zrobić spersonalizowane rpzyciski oraz proste ikony. Cały czas natomiast rozwijam te umiejętności, aby być częściowo niezależnym w tej dziedzinie</p>
+						</div>
+					</div>
+					<div class="row about-me-element">
+						<div class="col-md-6 col-sm-6 col-xs-12 text-center">
+							<p align="justify">W wolnych chwilach poświęcam się ćwiczeniom fizycznym oraz bieganiu. Bardzo lubię aktywność fizyczną. Dodatkowo działam w organizacji studenckiej, aby rozwinąć siebie nie tylko w kwestiach umiejętnośći twardych, ale również miekkich. Mogę również w ten sposób wpłynąć pozytywnie na innych</p>
+						</div>
+						<div class="col-md-6 col-sm-6 col-xs-12 text-center" >
+						<img src="img/aboutme3.png" class="about-img img-fluid " style="width: 40%">
+						</div>
+					</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="clear-space"></div>
+		<div class="parallax-container" data-parallax="scroll" data-bleed="10" data-speed="0.2" data-image-src="img/projects.jpeg"  style="height: 300px"></div>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-8 offset-md-2 col-sm-10 offset-sm-1 content">
+					<div  class=" content-div portfolio">
+						<h1 id="portfolio">Moje projekty</h1>
+						<div class="row">
+							<div class="col-md-6 col-sm-6 col-xs-12 text-center" style="border-right:1px solid #ddd;">
+								<a href=""><img src="img/portfolio1.png" class="img-fluid"></a>
+								<p align="justify">Mój pierwszy projekt, który wykonałem w ramach warsztatu oraz pogłębiania wiedzy z zakresu web developmentu, w którym pragnę się rozwijać. Planuję z biegiem czasu rozbudowywać tę stronę o nowe funkcjonalności, aby zdobywać nowe umiejętności w tej dziedzinie, jak również rozwijać umiejętności, które już posiadłem.</p>
+							</div>
+							<div class="col-md-6 col-sm-6 col-xs-12 text-center">
+								<a href="test.php" target="_blank"><img src="img/portfolio2.png" class="img-fluid"></a>
+								<p align="justify">Projekt, gdzie zajmowałem się budową portalu ogłoszeniowego w warstwie front-end. W głównej mierze odpowiadałem za większość podstron znajdujących się w danej domenie. Również byłem odpowiedzialny za responsywność tych stron. Prace te wykonywałem w ramach dwu miesięcznego stażu komercyjnego.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="clear-space"></div>
+		<div class="parallax-container" data-parallax="scroll" data-bleed="10" data-speed="0.2" data-image-src="img/contact.jpg"  style="height: 300px"></div>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-8 offset-md-2 col-sm-10 offset-sm-1 content">
+					<div  class=" content-div kontakt">
+						<h1 id="kontakt">Kontakt</h1>
+					<div class="row">
+						<div class="contact-me  col-md-6 col-sm-6 col-xs-12">
+						<form id="contact-form" method="POST" >
+							<div class="form-group">
+								<label>Imię: </label>
+								<input type="text" name="name" class="form-control ">
+								<label>Nazwisko: </label>
+								<input type="text" name="surname" class="form-control">
+								<label>E-mail: </label>
+								<input type="text" name="mail" class="form-control">
+								<label>Telefon: </label>
+								<input type="text" name="phone" class="form-control">
+								<label>Wiadomość: </label>
+								<textarea rows="7" name="message" class="form-control"></textarea>
+								<input type="submit" name="Submit" class=" btn btn-success pull-right send-btn">
+								<?php
+
+									if(isset($_POST["Submit"])) {
+									    $recipient="damiantomilo@gmail.com";
+									    $subject="Wiadomość ze strony tomilodesign.pl";
+									    $sender=$_POST["name"];
+									    $surname=$_POST["surname"];
+									    $phone=$_POST["phone"];
+									    $senderEmail=$_POST["mail"];
+									    $message=$_POST["message"];
+
+									    $mailBody="Imie: $sender\nNazwisko: $surname\nEmail: $senderEmail\nTelefon: $phone\nWiadomość: $message";
+
+									    mail($recipient, $subject, $mailBody, "From: $sender $senderEmail");
+									} 
+									?>
+							</div>
+						</form>
+						</div>
+						<div class="contact-info col-md-6 col-sm-6 col-xs-12 text-center">
+							<img src="img/phone-book.png" class="img-fluid contact-img">
+							<h4><i class="fa fa-envelope-o"></i> damiantomilo@gmail.com</h4>
+							<h4><i class="fa fa-phone"></i> 723-802-161</h4>
+						</div>
+					</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="clear-space"></div>
+		<div class="parallax-container" data-parallax="scroll" data-bleed="10" data-speed="0.2" data-image-src="img/end.jpg"  style="height: 300px"></div>
+	</body>
+
+
+
+	<script>
+	$(function () {
+	$('.tlt').textillate({
+		initialDelay: 300,
+		in:{
+			shuffle:true,
+			delayScale:3,
+		},
+	});
+});
+		$(function () {
+	$('.tlt2').textillate({
+		initialDelay: 1100,
+		in:{
+					delay:40,
+		},
+	});
+});
+		$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      var getHeaderHeight = $(".header_wrap").outerHeight();
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        if($('.nav').hasClass('sticky')){
+        	$('html, body').animate({
+          scrollTop: target.offset().top - 90
+        
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+        }else{
+        	$('html, body').animate({
+          scrollTop: target.offset().top - 95
+        
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+        }
+
+        
+      }
+    }
+  });
+	</script>
+
+	<script>
+$(document).ready(function() {
+var stickyNavTop = $('.nav').offset().top;
+ 
+var stickyNav = function(){
+var scrollTop = $(window).scrollTop();
+      
+if (scrollTop > stickyNavTop) { 
+    $('.nav').addClass('sticky');
+     $('.sticky').css('height','75px');
+
+} else {
+    $('.nav').removeClass('sticky'); 
+    $('.nav').animate({height:'2px'},1000);
+}
+}
+ 
+stickyNav();
+ 
+$(window).scroll(function() {
+  stickyNav();
+});
+});
+ 
+
+</script>
+</html>
